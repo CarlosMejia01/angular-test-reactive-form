e0cc4f52f98c356fafd54148f2595320cba280bb
Carlos Mejia <cmejia@equitel.com.co>
2022-03-12 01:26:20 -0500

    Upload project testing reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio-app';
+
+  formLogin!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      terms: ['', [Validators.required, Validators.requiredTrue]],
+    });
+
+
+    this.loadApi();
+
+  }
+
+  loadApi(): any {
+    const response = {
+      email: 'correoprueba@gmail.com',
+      password: '12345',
+      terms: true,
+    };
+
+    this.formLogin.patchValue({
+      email: response.email,
+      password: response.password,
+      terms: response.terms,
+    });
+  }
+
+  send() {
+    console.log(this.formLogin.value);
+  }
 }
